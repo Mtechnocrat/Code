ae468eccaab3e90baebed741350509df37b8e02d
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2025-06-25 15:59:31 +0530

    Code

--- JavaScript/01_basic/03_conversionType.js
+++ JavaScript/01_basic/03_conversionType.js
@@ -17,3 +17,17 @@ console.log(booleanLoggedIn); // true
 // 1 => true; 0=> false;
 //""=>false; 
 //"something" => true;
+
+
+// Operatioons
+
+console.log(3 + 4); // 7
+console.log("3" + 4); // "34"
+console.log(3 + "4"); // "34"   
+console.log("3" + "4"); // "34"
+
+console.log(1+3+"4"); // "44"
+console.log(1+"3"+true); // "13true"
+
+console.log(1+true); // 2
+console.log(1+false); // 1
